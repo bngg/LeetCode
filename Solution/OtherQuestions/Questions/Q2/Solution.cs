7433a4e646e92720927a150bf825f98f2fc1c2da
alaitang <tangalai@hotmail.com>
2016-02-27 20:42:06 -0800

    Add Q2

---
+++ Solution/OtherQuestions/Questions/Q2/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherQuestions.Questions.Q2
+{
+    public class Solution
+    {
+        public int GetMaxLengthOfProduct(string[] dicts)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
