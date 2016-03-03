cae55a2f1d7a9f9a51807d72ce6a72c041d64260
alaitang <tangalai@hotmail.com>
2016-03-02 16:45:14 -0800

    add Q3

---
+++ Solution/OtherQuestions/Questions/Q3/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherQuestions.Questions.Q3
+{
+    class Question
+    {
+        /*
+        sort char array without using any loop
+        */
+    }
+}
