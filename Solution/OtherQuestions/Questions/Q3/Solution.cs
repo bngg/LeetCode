e3fd48ae9c0436a4f26dad0f90b176b6f419e116
BinL <bng0517@gmail.com>
2016-03-03 10:42:22 -0800

    BinSortSolution

--- Solution/OtherQuestions/Questions/Q3/Solution.cs
+++ Solution/OtherQuestions/Questions/Q3/Solution.cs
@@ -10,7 +10,27 @@ namespace OtherQuestions.Questions.Q3
     {
         public void SortWithoutLoop(char[] input)
         {
-
+            sort(input, 0);
         }
+
+        private void swap(char[] nums, int i, int j)
+        {
+            char tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        public void sort(char[] A, int lev)
+        {
+            if (A.Length < 2 || lev == A.Length - 1)
+                return;
+
+            sort(A, lev + 1);
+            if (A[lev] > A[lev + 1])
+            {
+                swap(A, lev, lev + 1);
+                sort(A, lev + 1);
+            };
+        }
+
     }
 }
