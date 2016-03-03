9206410dff19ac9cc3c6acd0d4f223fcf74bf3eb
alaitang <tangalai@hotmail.com>
2016-03-02 17:01:16 -0800

    Solve question 3

--- Solution/OtherQuestions/Questions/Q3/Solution.cs
+++ Solution/OtherQuestions/Questions/Q3/Solution.cs
@@ -10,7 +10,32 @@ namespace OtherQuestions.Questions.Q3
     {
         public void SortWithoutLoop(char[] input)
         {
+            int len = input.Length;
 
+            SortFun(input, 0, 1, len);
+        }
+
+        private void SortFun(char[] arr, int low, int high, int len)
+        {
+            if (low >= len)
+            {
+                return;
+            }
+            else if (high >= len)
+            {
+                SortFun(arr, low + 1, low + 2, len);
+            }
+            else
+            {
+                if (arr[low] > arr[high])
+                {
+                    char c = arr[low];
+                    arr[low] = arr[high];
+                    arr[high] = c;
+                }
+
+                SortFun(arr, low, high + 1, len);
+            }
         }
     }
 }
